418d30d63eb70cff7f241a50d0201c83e9afc853
在虎 <caiths@icloud.com>
2023-06-04 10:40:52 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -33,8 +33,12 @@ export const getServerSideConfig = () => {
     );
   }
 
+  // 从这里开始
+  const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
+  const apiKey = apiKeys.at(Math.floor(Math.random() * apiKeys.length)) ?? ''
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    //原来的代码apiKey: process.env.OPENAI_API_KEY,
+    apiKey,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
